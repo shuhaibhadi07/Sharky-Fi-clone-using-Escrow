0bc172fcf82fabbd20cbd6f877fc2305d78fb7b3
shuhaibhadi07 <165528692+shuhaibhadi07@users.noreply.github.com>
2024-04-03 17:59:44 +0530

    lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("2PKuyGpkhV8ANYxSZeJoMxxYn8LN8eEzCHb5c2MashHw");
 
 #[program]
 pub mod nft_lend_borrow {
